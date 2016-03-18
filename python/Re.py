5f6d78922ba92c694543cd18e41715c5a43f8101
helio <helio@new-host.home>
2016-03-18 09:59:09 -0300

    func p/ calc numero de Reynolds

---
+++ python/Re.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 09:15:05 2016
+
+@author: helio
+"""
+
+def Re(vel,l,nu):
+    '''Calcula o numero de Reynolds como:
+    
+        vel*X/nu
+        
+    vel - velocidade (m/s)
+    
+    X   - comprimento característico (m)
+
+    nu = mu/rho - viscosidade cinemática (m2/s)
+
+    mu  - viscosidade dinámica (Pa·s or N·s/m2 or kg/(m·s))
+    
+    rho - densidade (kg/m3)
+    
+    '''
+    return vel*l/nu
